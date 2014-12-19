f0b01837f5df029d204ce1ece6323527058fb3ed
Przemek Mroczek <przemyslawmroczek@o2.pl>
2014-12-19 17:40:59 +0100

    Respect style guidelines for connection_spec.rb

--- spec/connection_spec.rb
+++ spec/connection_spec.rb
@@ -5,9 +5,9 @@ describe Dirble::Connection do
   let!(:status_query) { { request_type: :get, query: '/status' } }
 
   before do
-    stub_request(:get, "http://api.dirble.com/status").
-      with(:headers => {'User-Agent'=>'Faraday v0.9.0'}).
-      to_return(:status => 200, :body => "", :headers => {})
+    stub_request(:get, 'http://api.dirble.com/status')
+      .with(headers: { 'User-Agent' => 'Faraday v0.9.0' })
+      .to_return(status: 200, body: '', headers: {})
   end
 
   context 'valid params' do
@@ -19,8 +19,8 @@ describe Dirble::Connection do
   context 'invalid params' do
     it 'raises error on invalid request type' do
       invalid_request_type_query = status_query.merge(request_type: :poke)
-      expect {connection.exec_query(invalid_request_type_query)}.to raise_error
+      expect { connection.exec_query(invalid_request_type_query) }
+        .to raise_error
     end
   end
 end
-
